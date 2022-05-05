4477915d1cbb342a82fa6b22ed079288802dcf59
Kira Zero <longdragon02@gmail.com>
2022-05-05 15:26:18 +0700

    Add project files.

---
+++ FormTest/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace FormTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PlayBtn = new System.Windows.Forms.PictureBox();
+            this.btn_close = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PlayBtn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayBtn
+            // 
+            this.PlayBtn.BackColor = System.Drawing.Color.Transparent;
+            this.PlayBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PlayBtn.Image = global::FormTest.Properties.Resources.pixil_frame_01;
+            this.PlayBtn.Location = new System.Drawing.Point(331, 265);
+            this.PlayBtn.Name = "PlayBtn";
+            this.PlayBtn.Size = new System.Drawing.Size(211, 68);
+            this.PlayBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PlayBtn.TabIndex = 0;
+            this.PlayBtn.TabStop = false;
+            this.PlayBtn.Click += new System.EventHandler(this.playbtn_Click);
+            // 
+            // btn_close
+            // 
+            this.btn_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn_close.Location = new System.Drawing.Point(378, 441);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(121, 33);
+            this.btn_close.TabIndex = 1;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::FormTest.Properties.Resources.Title;
+            this.pictureBox2.Location = new System.Drawing.Point(169, 27);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(526, 133);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(883, 523);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.btn_close);
+            this.Controls.Add(this.PlayBtn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PlayBtn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PlayBtn;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ FormTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void playbtn_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("No game implemented yet !", "Under construction");
+        }
+    }
+}
